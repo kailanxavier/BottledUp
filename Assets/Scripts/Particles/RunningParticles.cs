0f404503e1c575749938d8f4e0dcbdf23ab0e671
ThomasG06 <thomfvgray@gmail.com>
2025-03-27 18:07:12 +0000

    Created a prototype of the running particle system.

--- Assets/Scripts/Particles/RunningParticles.cs
+++ Assets/Scripts/Particles/RunningParticles.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,14 @@ using UnityEngine;
 public class RunningParticles : MonoBehaviour
 {
     Player _player;
+    InputManager _inputManager;
+    ParticleSystem runningSystem;
 
     void Start()
     {
-        _player = GetComponentInChildren<Player>();
+        _player = GetComponentInParent<Player>();
+        _inputManager = GetComponentInParent<InputManager>();
+        runningSystem = GetComponent<ParticleSystem>();
     }
 
     void Update()
@@ -19,6 +24,16 @@ public class RunningParticles : MonoBehaviour
 
     void HandleParticles()
     {
+        Vector2 inputVector = _inputManager.GetInputVectorNormalized();
+
         bool isGrounded = _player.isGrounded;
+
+        // Check if player is moving
+        bool isMoving = Math.Abs(inputVector.x) > 0 || Math.Abs(inputVector.y) > 0;
+
+        if (isGrounded && isMoving) runningSystem.Play(); // If isMoving and isGrounded play the particles
+
+        Debug.Log("Is Moving: " + isMoving);
+        Debug.Log("Is Grounded: " + isGrounded);
     }
 }
