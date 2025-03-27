78cc2a3ef30b3b216f9276326a75878755d1e536
Kailan Xavier <kailanxavier@gmail.com>
2025-03-27 18:00:59 +0000

    more interaction work

--- Assets/Scripts/UI/InteractionUI.cs
+++ Assets/Scripts/UI/InteractionUI.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class InteractionUI : MonoBehaviour
 {
     public CustomCollision interactRangeCollider;
+    public string interactableTag = "Interactable";
     public float offsetAmount;
 
     private void Awake()
@@ -14,7 +15,10 @@ public class InteractionUI : MonoBehaviour
 
     private void OnInteractTriggerEntered(Collider collider)
     {
-        transform.position = collider.gameObject.transform.position + new Vector3(0, offsetAmount, 0);
+        if (collider.CompareTag(interactableTag))
+        {
+            transform.position = collider.gameObject.transform.position + new Vector3(0, offsetAmount, 0);
+        }
     }
 
 }
